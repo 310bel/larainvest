ca47f28afbc63ebe8a12c2a4b63a328b84439188
Ilchenko Evgeniy <ilchenko_e@bsu.edu.ru>
2022-09-22 18:25:45 +0300

    php artisan make:controller dashboardController --resource

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
 require __DIR__.'/auth.php';
+
+
+Route::get('/home', [dashboardController::class, 'index']);
