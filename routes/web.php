37d5f32071639954e6282caa73d1f51b3afe8d42
fred <310bel@gmail.com>
2022-09-26 01:05:34 +0300

    добавил дома command laravel.txt

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -24,6 +13,5 @@ Route::get('/dashboard', function () {
 
 
 require __DIR__.'/auth.php';
-
 
 Route::get('/home', [dashboardController::class, 'index']);
