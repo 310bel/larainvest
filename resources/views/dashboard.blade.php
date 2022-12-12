ab82c6c6f54269e45994c106de2c5e0d5cd96377
Ilchenko_e <lipko@bsu.edu.ru>
2022-12-12 11:00:41 +0300

    Львович лич кабинет2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,7 +12,7 @@
                     <div class="p-6 bg-white border-b border-gray-200">
 
                         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                            Итого {{$total}} р.
+                            Итогооо {{$total}} р.
                         </h2>
                         <br>
 
