5bd90ab83d734bc9fe35a081af45951c97c421ea
Ilchenko_e <lipko@bsu.edu.ru>
2022-12-26 18:16:25 +0300

    getbootstrap_3

--- resources/views/lvovich/index.blade.php
+++ resources/views/lvovich/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header"></x-slot>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto  ">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
 
@@ -10,8 +10,7 @@
                             Итого {{$total}} р.
                         </h2>
                         <br>
-
-        <table class="table table-responsive table-sm table-striped table-bordered" >
+        <table class="table table-sm table-striped table-bordered table-hover " >
             <tr>
                 <th scope="col">Дата </th>
                 <th scope="col">Транзакция </th>
@@ -27,8 +26,9 @@
             @endforeach
                     </table>
                         <a href="{{ route('lvovich.create') }}" >Добавить</a>
+
+                    </div>
                 </div>
             </div>
-        </div>
     </div>
 </x-app-layout>
