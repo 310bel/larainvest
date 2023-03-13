d83929df7d23b858aa466ad9fb4848eedd80bb2b
Ilchenko_e <lipko@bsu.edu.ru>
2023-03-13 17:46:58 +0300

    добавил Masonry

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -1,49 +1,54 @@
-<x-app-layout>
-    <x-slot name="header"></x-slot>
+{{--<x-app-layout>--}}
+{{--    <x-slot name="header"></x-slot>--}}
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto  ">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/masonry-layout@4.2.2/dist/masonry.pkgd.min.js" integrity="sha384-GNFwBvfVxBkLMJpYMOABq3c+d3KnQxudP/mGPkzpZSTYykLBNsZEnG2D9G/X/+7D" crossorigin="anonymous" async></script>
+    <title>gallery</title>
 
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-                        <br>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-                        <table class="table table-sm table-striped table-bordered table-hover ">
-                            <tr>
-                                @foreach($pazov as $key=>$item)
-                                <td>
-                                    <img src="{{url('storage/' . $item->image)}} ">
-                                </td>
-                                @if(++$key % 3==0)
-                            </tr><tr>
-                                @endif
-                                @endforeach
-                            </tr>
-                        </table>
+{{--                        <br>--}}
+{{--<div class="row" data-masonry='{"percentPosition": true }'>--}}
+{{--                        <table >--}}
+{{--                                @foreach($pazov as $key=>$item)--}}
+{{--                                <td  >--}}
+{{--                                    <img  src="{{url('storage/' . $item->image)}}  "class="img-thumbnail"><br>{{ $item->product }}<br>код товара: {{ $item->code }}<br>цена  {{ $item->price }} р.--}}
+{{--                                </td>--}}
+{{--                                @if(++$key % 4==0)--}}
+{{--                            </tr><tr>--}}
+{{--                                @endif--}}
+{{--                                @endforeach--}}
+{{--                        </table>--}}
+{{--</div>--}}
 
+<div class="container">
+    <div class="row" data-masonry='{"percentPosition": true }'>
 
-        <table class="table table-sm table-striped table-bordered table-hover " >
-            @for ($i = 0; $i < 10; $i++)
-{{--            {{$pazov_array}};--}}
-{{--            @foreach($pazov as $item)--}}
-{{--            @php($new_date_format = date('d-m-Y', strtotime($item->date)))--}}
-                <tr>
-{{--                    <td><a href="{{ route('pazov.show', $item->id) }}" >{{ $item->product }}</td>--}}
-{{--                    <td><a href="{{ route('pazov.show', $item->id) }}" >{{ $item->code }}</td>--}}
-{{--                    <td><a href="{{ route('pazov.show', $item->id) }}" >{{ $item->price }}</td>--}}
-{{--                    <td class="w-5" ><img src="{{url('storage/' . $pazov_array[$i])}} ">--}}
-                    <td class="w-5" ><img src="{{url('storage/' . $item->image)}} ">
-                </tr>
-            @endfor
-{{--            @endforeach--}}
-                    </table>
-                        <a href="{{ route('pazov.create') }}" >Добавить</a>
+        @foreach($pazov as $key=>$item)
 
-                    </div>
+            <div class="col-xxl-1 col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12">
+                <div class="card">
+                    <img src="{{url('storage/' . $item->image)}}  " class="img-thumbnail">
+                    <h12 class='cart-title'>{{ $item->product }}</h12>
+                    <br>
+                    код товара: {{ $item->code }}<br>цена {{ $item->price }} р.
                 </div>
             </div>
+
+        @endforeach
     </div>
-</x-app-layout>
+</div>
+
+
+{{--</x-app-layout>--}}
 
 
